c1d510f04e6ce0e4a4e2f831a301ba7009ab58fe
miguel-mendez-martinez <miguetripleM@gmail.com>
2023-04-18 10:51:09 +0200

    está todo menos cargar

--- system.c
+++ system.c
@@ -25,7 +25,44 @@ double salir(){
 }
 
 double help(){
-    printf("Bienvenido al interprete matematico perra.\n");
+    printf("\n===================================================================================================================================================\n\n"
+           "\t\t\t\t\t\t\t\t\tHELP\n\n"
+           "---------------------------------------------------------------------------------------------------------------------------------------------------\n\n"
+           "\t  \n"
+           "\t\t  \n"
+           "\t\t  \n"
+           "\t\t  \n\n"
+           "\t\t  \n\n"
+           "\t\t  \n"
+           "\t\t  \n"
+           "\t\t  \n"
+           "\t\t  \n\n"
+           "\t\t  \n"
+           "\t\t  \n"
+           "\t\t  \n\n\n"
+           "\tCOMANDOS\n"
+           "\t\tPara a execución de comandos hai dous tipos de sintaxe: con paréntesis ou sen eles. Nos comandos sen parámetros sería 'CMND'\n"
+           "\t\tou 'CMND()', respectivamente; e nos comandos cun parámetro sería 'CMND PARAM' ou 'CMND(PARAM)', respectivamente.\n\n"
+           "\t\tOs comandos dispoñibles son:\n\n"
+           "\t\t\t- axuda\t\t\t\tMostra esta axuda. Tamén se pode mostar ao iniciar o propio intérprete indicando a\n"
+           "\t\t\t\t\t\t\topción '-h': './nicalc -h'.\n\n"
+           "\t\t\t- cargar FICHEIRO\t\tCarga e executa un ficheiro.\n"
+           "\t\t\t\t\t\t\tEste ficheiro debe respectar a sintaxe do intérprete e non poderá cargar outros\n"
+           "\t\t\t\t\t\t\tficheiros desde el (en caso de que o faga non se asegura o correcto funcionamento\n"
+           "\t\t\t\t\t\t\tdo intérprete).\n"
+           "\t\t\t\t\t\t\tTamén se poden executar ficheiros ao iniciar o propio intérprete inidicándoo como\n"
+           "\t\t\t\t\t\t\targumento: './nicalc FICHEIRO'.\n\n"
+           "\t\t\t- clear\t\t\t\tLimpa a terminal de saída.\n\n"
+           "\t\t\t- echo\t\t\t\tActiva/Desactiva a impresión da información de saída. Por defecto está activada.\n\n"
+           "\t\t\t- importar LIBRERIA\t\tImporta unha librería de forma dinámica a través do seu ficheiro .so.\n"
+           "\t\t\t\t\t\t\tSe se indica só o nome, buscarase por orde nas rutas /lib e /usr/lib, non obstante\n"
+           "\t\t\t\t\t\t\tpódense importar librerías indicando a ruta completa do seu ficheiro .so.\n\n"
+           "\t\t\t- limparws\t\t\tLimpa o espazo de traballo.\n\n"
+           "\t\t\t- sair\t\t\t\tSae do intérprete.\n\n"
+           "\t\t\t- taboa\t\t\t\tMostra a táboa de símbolos, que contén os comandos, constantes, funcións usadas\n"
+           "\t\t\t\t\t\t\talgunha vez, librerías importadas e variables.\n\n"
+           "\t\t\t- ws\t\t\t\tMostrar o espazo de traballo actual, que contén as variables definidas ata o momento.\n\n"
+           "\n===================================================================================================================================================\n\n");
     return 0;
 }
 
@@ -56,6 +93,7 @@ double eliminarWs(){
 }
 
 double cargar(char *fichero){
+    printf("He llegado a cargar el fichero\n");
     yyin = fopen(fichero, "r");
 
     if(yyin == NULL){
